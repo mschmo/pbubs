141ca1043aa001e0d68df1b2210f3acc40bf11e0
Matt Schmoyer <mattschmo@gmail.com>
2015-10-25 17:38:57 -0400

    Add bill model

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,2 +1,4 @@
 from .accepted_emails import AcceptedEmail
+from .bill import Bill
+from .bill_type import BillType
 from .user import User
---
+++ server/models/bill.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from server.db import db, ActiveModel
+
+
+class Bill(ActiveModel, db.Model):
+    __tablename__ = 'bills'
+
+    id = db.Column(db.Integer, primary_key=True)
+    type_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.String(128), unique=True, nullable=False)
+    amount = db.Column(db.DECIMAL(5, 2), nullable=False)
+    active = db.Column(db.Boolean, nullable=False, default=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Bill {}>'.format(self.id)
