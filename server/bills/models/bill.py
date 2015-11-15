adaa538f02268ace18cd647bbceb198ccec7b4ae
Matt Schmoyer <mattschmo@gmail.com>
2015-11-15 18:21:02 -0500

    Add foreign keys

--- server/bills/models/bill.py
+++ server/bills/models/bill.py
@@ -6,12 +6,14 @@ class Bill(ActiveModel, db.Model):
     __tablename__ = 'bills'
 
     id = db.Column(db.Integer, primary_key=True)
-    type_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
+    type_id = db.Column(db.Integer, db.ForeignKey('bill_types.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     comment = db.Column(db.String(128), unique=True, nullable=False)
     amount = db.Column(db.DECIMAL(5, 2), nullable=False)
     active = db.Column(db.Boolean, nullable=False, default=True)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    bill_type = db.relationship('BillType', foreign_keys='Bill.type_id', uselist=False, backref='events')
 
     @property
     def pretty_date(self):
